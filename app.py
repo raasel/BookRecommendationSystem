72a3174c0cb79b7cd15facb7128caa3712555f80
Md Rasel Ahmed <47408774+raasel@users.noreply.github.com>
2023-09-12 01:09:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,12 +14,7 @@ import importfile
 
 
 ###############--------code for Dataset import and Data Preprocessing-------------###################
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
+
 
 ###############--------End for Dataset import and Data Preprocessing-------------###################
 
